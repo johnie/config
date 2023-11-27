c77472cbb53dddfee6f7618a4fd0b5334b39a7c8
Johnie Hjelm <johnie@hjelm.im>
2023-11-27 08:09:57 +0100

    feat: Add scripts

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -2,3 +2,13 @@ while read p; do
   echo Installing $p
   code --install-extension $p
 done < ../init/vscode_extensions.json
+
+$vscodeDirectory = "$HOME/Library/Application Support/Code/User"
+
+echo "Backup VSCode settings..."
+
+mv "$vscodeDirectory/settings.json" "$vscodeDirectory/settings.json.backup"
+
+echo "Symlinking VSCode settings..."
+
+ln -sf "../init/vscode_settings.json" "$vscodeDirectory/settings.json"
